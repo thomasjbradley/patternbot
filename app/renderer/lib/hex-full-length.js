d7e7c26aa764e39dbd7235809d6f835e1514378b
Thomas J Bradley <hey@thomasjbradley.ca>
2018-01-25 16:52:24 -0500

    Fix a couple bugs in the hex full length generator

--- app/renderer/lib/hex-full-length.js
+++ app/renderer/lib/hex-full-length.js
@@ -2,8 +2,8 @@ module.exports = function (hex) {
   if (hex.length === 7) return hex;
 
   if (hex.length === 4) {
-    return `#${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[4]}`;
+    return `#${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[3]}`;
   }
 
-  return hex.padEnd('0');
+  return hex.padEnd(7, '0');
 };
