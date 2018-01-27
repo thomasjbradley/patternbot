6862e54a92f468681d18b057fe8e4f81dce06a75
Thomas J Bradley <hey@thomasjbradley.ca>
2018-01-27 16:00:37 -0500

    Reorganize some code for clarity

--- app/renderer/lib/readme-parser.js
+++ app/renderer/lib/readme-parser.js
@@ -7,6 +7,20 @@ const markdownFileParser = require(`${__dirname}/markdown-file-parser`);
 
 const README_FILENAME = 'README.md';
 
+// Allow both correct & American spelling
+const normalizeSpelling = function (readme) {
+  if (readme.attributes.colors) readme.attributes.colours = readme.attributes.colors;
+
+  if (readme.attributes.typekit && /=/.test(readme.attributes.typekit)) readme.attributes.typekitId = readme.attributes.typekit.split(/=/)[0];
+
+  if (readme.attributes.backgroundColor) readme.attributes.backgroundColour = readme.attributes.backgroundColor;
+  if (!readme.attributes.backgroundColour) readme.attributes.backgroundColour = '#fff';
+
+  if (readme.attributes.accentColor) readme.attributes.accentColour = readme.attributes.accentColor;
+
+  return readme
+};
+
 const parse = function (folderpath) {
   return new Promise((resolve, reject) => {
     const readmepath = `${folderpath}/${README_FILENAME}`;
@@ -16,15 +30,7 @@ const parse = function (folderpath) {
     fs.readFile(readmepath, 'utf8', (err, data) => {
       let readme = markdownFileParser(data);
 
-      // Allow both correct & American spelling
-      if (readme.attributes.colors) readme.attributes.colours = readme.attributes.colors;
-
-      if (readme.attributes.typekit && /=/.test(readme.attributes.typekit)) readme.attributes.typekitId = readme.attributes.typekit.split(/=/)[0];
-
-      if (readme.attributes.backgroundColor) readme.attributes.backgroundColour = readme.attributes.backgroundColor;
-      if (!readme.attributes.backgroundColour) readme.attributes.backgroundColour = '#fff';
-
-      if (readme.attributes.accentColor) readme.attributes.accentColour = readme.attributes.accentColor;
+      readme = normalizeSpelling(readme);
 
       resolve(readme);
     });
